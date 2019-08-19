ce7be24a4b4e442bde93ccc4238a8e7cd511a080
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-08-19 16:55:45 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -173,7 +173,7 @@ class PaymentService
             'payment_name'     => $nnPaymentData['payment_method'],
             'order_no'         => $nnPaymentData['order_no'],
             'bank_details'	   => !empty($bank_details) ? json_encode($bank_details) : '0',
-	    'transaction_details' => json_encode($transactionComments);
+	    'transaction_details' => json_encode($transactionComments)
         ];
 	   
 	    
