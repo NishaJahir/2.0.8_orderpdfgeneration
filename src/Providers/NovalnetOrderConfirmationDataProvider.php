56e16541c4921490d3a559622367b19cabb64816
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-08-19 14:22:47 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -42,7 +42,7 @@ class NovalnetOrderConfirmationDataProvider
 	 * @param Arguments $arg
 	 * @return string
 	 */
-	public function call(Twig $twig, PaymentRepositoryContract $paymentRepositoryContract, $arg)
+	public function call(Twig $twig, PaymentRepositoryContract $paymentRepositoryContract, $arg, DataBase $database)
 	{
 		$paymentHelper = pluginApp(PaymentHelper::class);
 		$sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
