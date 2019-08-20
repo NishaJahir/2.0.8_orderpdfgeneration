12e84cb1a1c4ab919442588efcedffe1c7e453b3
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-08-20 19:43:09 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -48,22 +48,21 @@ class NovalnetOrderConfirmationDataProvider
 		$barzhlentoken = '';
 		$barzahlenurl = '';
 		$payments = $paymentRepositoryContract->getPaymentsByOrderId($order['id']);
-		$paymentHelper->logger('dataprovider1', $payments);
 		if (!empty ($order['id'])) {
 			foreach($payments as $payment)
 			{
-					$properties = $payment->properties;
-					foreach($properties as $property)
-					{
-					if ($property->typeId == 21) 
-					{
-					$invoiceDetails = $property->value;
-					}
-					if ($property->typeId == 30)
-				  	{
-					$tid_status = $property->value;
-				 	}
-					}
+				$properties = $payment->properties;
+				foreach($properties as $property)
+				{
+				if ($property->typeId == 21) 
+				{
+				$invoiceDetails = $property->value;
+				}
+				if ($property->typeId == 30)
+				{
+				$tid_status = $property->value;
+				}
+				}
 				if($paymentHelper->getPaymentKeyByMop($payment->mopId))
 				{
 					if ($payment->method['paymentKey'] == 'NOVALNET_CASHPAYMENT')
@@ -75,24 +74,20 @@ class NovalnetOrderConfirmationDataProvider
 					
 					$comment = '';
 					$db_details = $paymentService->getDatabaseValues($orderId);
-					$bank_details = array_merge($db_details, json_decode($invoiceDetails, true));
 					$comments = '';
-					$comments .= PHP_EOL . $paymentHelper->getTranslatedText('nn_tid') . $bank_details['tid'];
-					//if(!empty($bank_details['test_mode'])) {
+					$comments .= PHP_EOL . $paymentHelper->getTranslatedText('nn_tid') . $db_details['tid'];
 					$comments .= PHP_EOL . $paymentHelper->getTranslatedText('test_order');
-					//}
-					$paymentHelper->logger('dataprovider', $bank_details);
+					$bank_details = array_merge($db_details, json_decode($invoiceDetails, true));
 					if (in_array($bank_details['paymentName'], ['novalnet_invoice', 'novalnet_prepayment'])) {
-					        $comments = '';
 						$comments .= PHP_EOL . $paymentService->getInvoicePrepaymentComments($bank_details);
 					} 
 				}
-					
+		}
+
 					$comment .= (string) $comments;
 					$comment .= PHP_EOL;
 					
 					}	
-					
 
 				  $payment_type = (string)$paymentHelper->getPaymentKeyByMop($payment->mopId);
 				  return $twig->render('Novalnet::NovalnetOrderHistory', ['comments' => html_entity_decode($comment),'barzahlentoken' => $barzhlentoken,'payment_type' => html_entity_decode($payment_type),'barzahlenurl' => $barzahlenurl]);
