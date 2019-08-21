0949b938fc0771e1103916c626753717fb651096
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-08-21 11:35:48 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -326,13 +326,13 @@ class PaymentService
                 $strnos++;
             }
         }
-	storedetails[] = '';
+	$storedetails[] = '';
         for($i = 1; $i <= $strnos; $i++)
         {
-	storedetails[$i]['nearest_store_country']= $requestData['nearest_store_country_' . $i];
-	storedetails[$i]['nearest_store_title']= $requestData['nearest_store_title_' . $i];
-	storedetails[$i]['nearest_store_city']= $requestData['nearest_store_city_' . $i];
-	storedetails[$i]['nearest_store_zipcode']= $requestData['nearest_store_zipcode_' . $i];
+	$storedetails[$i]['nearest_store_country']= $requestData['nearest_store_country_' . $i];
+	$storedetails[$i]['nearest_store_title']= $requestData['nearest_store_title_' . $i];
+	$storedetails[$i]['nearest_store_city']= $requestData['nearest_store_city_' . $i];
+	$storedetails[$i]['nearest_store_zipcode']= $requestData['nearest_store_zipcode_' . $i];
             //$countryName = !empty($requestData['nearest_store_country_' . $i]) ? $requestData['nearest_store_country_' . $i] : '';
           //  $comments .= $requestData['nearest_store_title_' . $i] . PHP_EOL;
           //  $comments .= $countryName . PHP_EOL;
@@ -341,7 +341,7 @@ class PaymentService
           //  $comments .= $requestData['nearest_store_zipcode_' . $i] . PHP_EOL . PHP_EOL;
         }
 
-        return storedetails;
+        return $storedetails;
     }
 
     /**
